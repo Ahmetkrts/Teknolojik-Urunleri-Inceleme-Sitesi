3d77387abb1253b7968abe9d960ac561a62a6d97
Enes Çopoglu <63410697+enes4181@users.noreply.github.com>
2020-04-11 22:26:03 +0300

    Revert "project pertama"
    This reverts commit b041297d1556db3cc02675439f581e651882b04c.

--- src/java/controller/telefonController.java
+++ src/java/controller/telefonController.java
@@ -5,10 +5,9 @@
  */
 package controller;
 
-import dao.telefonDAO;
+import dao.telefonDao;
 import entity.telefon;
 import java.io.Serializable;
-import java.sql.SQLException;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -19,25 +18,11 @@ import javax.inject.Named;
  */
 @Named
 @SessionScoped
-public class telefonController implements Serializable {
-
+public class telefonController implements Serializable{
+    
     private List<telefon> telefon_list;
-    private telefonDAO telefondao;
+    private telefonDao telefondao;
     private telefon telefon;
-    private Long selectEkran;
-    private Long selectBatarya;
-    private Long selectIslemci;
-    private Long selectArkaKamera;
-    private Long selectOnKamera;
-    private Long selectIsletimSistemi;
-    private List<Long> selectRenkList;
-
-   
-
-    public void create()  {
-        this.getTelefondao().insert(this.telefon, this.selectEkran,this.selectBatarya,this.selectIslemci,this.selectArkaKamera,this.selectOnKamera,this.selectIsletimSistemi,this.selectRenkList);
-        
-    }
 
     public List<telefon> getTelefon_list() {
         this.telefon_list = this.getTelefondao().findAll();
@@ -48,19 +33,19 @@ public class telefonController implements Serializable {
         this.telefon_list = telefon_list;
     }
 
-    public telefonDAO getTelefondao() {
-        if (this.telefondao == null) {
-            this.telefondao = new telefonDAO();
+    public telefonDao getTelefondao() {
+        if(this.telefondao == null){
+            this.telefondao = new telefonDao();
         }
         return telefondao;
     }
 
-    public void setTelefondao(telefonDAO telefondao) {
+    public void setTelefondao(telefonDao telefondao) {
         this.telefondao = telefondao;
     }
 
     public telefon getTelefon() {
-        if (this.telefon == null) {
+        if(this.telefon == null){
             this.telefon = new telefon();
         }
         return telefon;
@@ -69,60 +54,5 @@ public class telefonController implements Serializable {
     public void setTelefon(telefon telefon) {
         this.telefon = telefon;
     }
-
-    public Long getSelectEkran() {
-        return selectEkran;
-    }
-
-    public void setSelectEkran(Long selectEkran) {
-        this.selectEkran = selectEkran;
-    }
-
-    public Long getSelectBatarya() {
-        return selectBatarya;
-    }
-
-    public void setSelectBatarya(Long selectBatarya) {
-        this.selectBatarya = selectBatarya;
-    }
-
-    public Long getSelectIslemci() {
-        return selectIslemci;
-    }
-
-    public void setSelectIslemci(Long selectIslemci) {
-        this.selectIslemci = selectIslemci;
-    }
-
-    public Long getSelectArkaKamera() {
-        return selectArkaKamera;
-    }
-
-    public void setSelectArkaKamera(Long selectArkaKamera) {
-        this.selectArkaKamera = selectArkaKamera;
-    }
-
-    public Long getSelectOnKamera() {
-        return selectOnKamera;
-    }
-
-    public void setSelectOnKamera(Long selectOnKamera) {
-        this.selectOnKamera = selectOnKamera;
-    }
-
-    public Long getSelectIsletimSistemi() {
-        return selectIsletimSistemi;
-    }
-
-    public void setSelectIsletimSistemi(Long selectIsletimSistemi) {
-        this.selectIsletimSistemi = selectIsletimSistemi;
-    }
-     public List<Long> getSelectRenkList() {
-        return selectRenkList;
-    }
-
-    public void setSelectRenkList(List<Long> selectRenkList) {
-        this.selectRenkList = selectRenkList;
-    }
-
+    
 }
